686388e67c0e714607308eebed606b983dce7b2b
Surya <ayrusranos@gmail.com>
2021-03-08 05:23:29 +0530

    removing firebase config file

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
-import {firebaseConfig} from './firebaseconfig'
+import {firebaseConfig} from './config'
  
+
+
+// var Config = {
+//   apiKey: "config.firebase.apiKey",
+//   authDomain: "config.firebase.authDomain",
+//   projectId: "config.firebase.projectId",
+//   storageBucket: "config.firebase.storageBucket",
+//   messagingSenderId: "config.firebase.messagingSenderId",
+//   appId: "config.firebase.appId"
+// };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
